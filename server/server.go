b2d47d27eec53dbb61cd8938d0eef4f63a7b3e8b
Olusola Alao <alaoolusolae@gmail.com>
2022-01-20 03:54:47 +0100

    create server

---
+++ server/server.go
@@ -0,0 +1,125 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/olusolaa/inventry/cache"
+	"github.com/olusolaa/inventry/db"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+// Server serves requests to DB with router
+type Server struct {
+	DB    db.DB
+	Cache cache.Cache
+}
+
+// create routers for inventory of items
+func (s *Server) defineRoutes(r *gin.Engine) {
+	itemRouter := r.Group("/api/v1")
+	itemRouter.GET("items/", s.GetItems())
+	itemRouter.GET("/item/:id", s.GetItem())
+	itemRouter.POST("/items/", s.CreateItem())
+	itemRouter.PUT("items/:id", s.UpdateItem())
+	itemRouter.DELETE("items/:id", s.DeleteItem())
+
+	router := r.Group("/api/v1")
+	router.GET("/categories", s.GetCategories())
+	router.POST("/categories/", s.CreateCategory())
+	router.PUT("/categories/:id", s.UpdateCategory())
+	router.DELETE("/categories/:id", s.DeleteCategory())
+	router.GET("/categories/:id/items", s.GetCategoryItems())
+
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+}
+
+func (s *Server) setupRouter() *gin.Engine {
+	ginMode := os.Getenv("GIN_MODE")
+	if ginMode == "test" {
+		r := gin.New()
+		s.defineRoutes(r)
+		return r
+	}
+	r := gin.New()
+	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
+	// setup cors
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "PATCH"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	s.defineRoutes(r)
+	return r
+}
+
+// Start starts the whole server by preparing everything it needs
+// like router
+func (s *Server) Start() {
+	r := s.setupRouter()
+	PORT := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if PORT == ":" {
+		PORT = ":8080"
+	}
+	srv := &http.Server{
+		Addr:    PORT,
+		Handler: r,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	log.Printf("Server started on %s\n", PORT)
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}
